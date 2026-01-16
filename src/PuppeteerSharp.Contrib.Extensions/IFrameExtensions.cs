f7719300cc57b6e3c23b62a834523ae0d8c5d1e6
George Volsky <rextextau@gmail.com>
2026-01-16 12:19:41 +0300

    [+] Missing methods for IFrameExtensions

--- src/PuppeteerSharp.Contrib.Extensions/IFrameExtensions.cs
+++ src/PuppeteerSharp.Contrib.Extensions/IFrameExtensions.cs
@@ -10,6 +10,96 @@ namespace PuppeteerSharp.Contrib.Extensions
     public static class IFrameExtensions
     {
         /// <summary>
+        /// The method runs <c>document.querySelectorAll</c> within the iframe and then tests a <c>RegExp</c> against the elements <c>textContent</c>. The first element match is returned. If no element matches the selector and regular expression, the return value resolve to <c>null</c>.
+        /// </summary>
+        /// <param name="iframe">An <see cref="IFrame"/> to query.</param>
+        /// <param name="selector">A selector to query iframe for.</param>
+        /// <param name="regex">A regular expression to test against <c>element.textContent</c>.</param>
+        /// <param name="flags">A set of flags for the regular expression.</param>
+        /// <returns>Task which resolves to an <see cref="IElementHandle"/> pointing to the frame element.</returns>
+        /// <seealso href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp"/>
+        public static async Task<IElementHandle?> QuerySelectorWithContentAsync(this IFrame iframe, string selector, string regex, string flags = "")
+        {
+            return await iframe.GuardFromNull().EvaluateFunctionHandleAsync(
+                @"(selector, regex, flags) => {
+                    var elements = document.querySelectorAll(selector);
+                    return Array.prototype.find.call(elements, function(element) {
+                        return RegExp(regex, flags).test(element.textContent);
+                    });
+                }",
+                selector,
+                regex,
+                flags).ConfigureAwait(false) as IElementHandle;
+        }
+
+        /// <summary>
+        /// The method runs <c>document.querySelectorAll</c> within the iframe and then tests a <c>RegExp</c> against the elements <c>textContent</c>. All element matches are returned. If no element matches the selector and regular expression, the return value resolve to <see cref="Array.Empty{T}"/>.
+        /// </summary>
+        /// <param name="iframe">An <see cref="IFrame"/> to query.</param>
+        /// <param name="selector">A selector to query iframe for.</param>
+        /// <param name="regex">A regular expression to test against <c>element.textContent</c>.</param>
+        /// <param name="flags">A set of flags for the regular expression.</param>
+        /// <returns>Task which resolves to an <see cref="IElementHandle"/> array pointing to the frame elements.</returns>
+        /// <seealso href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp"/>
+        public static async Task<IElementHandle[]> QuerySelectorAllWithContentAsync(this IFrame iframe, string selector, string regex, string flags = "")
+        {
+            var arrayHandle = await iframe.GuardFromNull().EvaluateFunctionHandleAsync(
+                @"(selector, regex, flags) => {
+                    var elements = document.querySelectorAll(selector);
+                    return Array.prototype.filter.call(elements, function(element) {
+                        return RegExp(regex, flags).test(element.textContent);
+                    });
+                }",
+                selector,
+                regex,
+                flags).ConfigureAwait(false);
+
+            var properties = await arrayHandle.GetPropertiesAsync().ConfigureAwait(false);
+            await arrayHandle.DisposeAsync().ConfigureAwait(false);
+
+            return properties.Values.OfType<IElementHandle>().ToArray();
+        }
+
+        /// <summary>
+        /// Indicates whether the iframe has the specified content or not.
+        /// </summary>
+        /// <param name="iframe">An <see cref="IFrame"/> to query.</param>
+        /// <param name="regex">A regular expression to test against <c>document.documentElement.textContent</c>.</param>
+        /// <param name="flags">A set of flags for the regular expression.</param>
+        /// <returns><c>true</c> if the iframe has the specified content.</returns>
+        /// <seealso href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp"/>
+        public static async Task<bool> HasContentAsync(this IFrame iframe, string regex, string flags = "")
+        {
+            return await iframe.GuardFromNull().EvaluateFunctionAsync<bool>("(regex, flags) => RegExp(regex, flags).test(contentDocument.documentElement.textContent)", regex, flags).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Indicates whether the iframe has the specified title or not.
+        /// </summary>
+        /// <param name="iframe">An <see cref="IFrame"/> to query.</param>
+        /// <param name="regex">A regular expression to test against <c>document.title</c>.</param>
+        /// <param name="flags">A set of flags for the regular expression.</param>
+        /// <returns><c>true</c> if the iframe has the specified title.</returns>
+        /// <seealso href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp"/>
+        public static async Task<bool> HasTitleAsync(this IFrame iframe, string regex, string flags = "")
+        {
+            return await iframe.GuardFromNull().EvaluateFunctionAsync<bool>("(regex, flags) => RegExp(regex, flags).test(document.title)", regex, flags).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Indicates whether the iframe has the specified URL or not.
+        /// </summary>
+        /// <param name="iframe">An <see cref="IFrame"/> to query.</param>
+        /// <param name="regex">A regular expression to test against <c>window.location.href</c>.</param>
+        /// <param name="flags">A set of flags for the regular expression.</param>
+        /// <returns><c>true</c> if the iframe has the specified URL.</returns>
+        /// <seealso href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp"/>
+        public static async Task<bool> HasUrlAsync(this IFrame iframe, string regex, string flags = "")
+        {
+            return await iframe.GuardFromNull().EvaluateFunctionAsync<bool>("(regex, flags) => RegExp(regex, flags).test(contentWindow.location.href)", regex, flags).ConfigureAwait(false);
+        }
+
+        /// <summary>
         /// Waits for the specific element to be removed from iframe's DOM.
         /// </summary>
         /// <param name="iframe">A <see cref="IFrame"/>.</param>
